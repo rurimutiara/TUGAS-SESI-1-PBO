66634158fb433d11d9dbf20712f80ea1c849a207
rurimutiara <114370768+rurimutiara@users.noreply.github.com>
2022-09-26 09:36:25 +0700

    Add files via upload

---
+++ BelajarJava.java
@@ -0,0 +1,12 @@
+package belajarjava;
+
+public class BelajarJava {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+    
+}
+        
+
